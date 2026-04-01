e64a249e574841ab0267be85ea8e1e1349f37ce6
lukejj1 <lucasdiasvieira77@gmail.com>
2026-04-01 11:13:24 -0400

    Update CandidaturaReposistory.java

--- src/main/java/com/matchvagas/repository/CandidaturaReposistory.java
+++ src/main/java/com/matchvagas/repository/CandidaturaReposistory.java
@@ -7,30 +7,30 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.matchvagas.backend.entity.Candidatura;
+import com.matchvagas.backend.entity.Candidaturas;
 
 @Repository
-public interface CandidaturaRepository extends JpaRepository<Candidatura, Long> {
+public interface CandidaturaRepository extends JpaRepository<Candidaturas, Long> {
 
     // Busca por status da candidatura
-    List<Candidatura> findByStatus(String status);
+    List<Candidaturas> findByStatus(String status);
 
     // Busca por id do candidato
-    List<Candidatura> findByCandidatoId(Long candidatoId);
+    List<Candidaturas> findByCandidatoId(Long candidatoId);
 
     // Busca por id da vaga
-    List<Candidatura> findByVagaId(Long vagaId);
+    List<Candidaturas> findByVagaId(Long vagaId);
 
     // Busca por email do candidato (assumindo que CandidatoVaga tem campo email)
-    List<Candidatura> findByCandidatoEmail(String email);
+    List<Candidaturas> findByCandidatoEmail(String email);
 
 
     @EntityGraph(attributePaths = {"candidato", "candidato.formacoes", "candidato.experiencias", "vaga"})
-    List<Candidatura> findAll();
+    List<Candidaturas> findAll();
 
     @EntityGraph(attributePaths = {"candidato", "candidato.formacoes", "candidato.experiencias", "vaga"})
-    Optional<Candidatura> findById(Long id);
+    Optional<Candidaturas> findById(Long id);
 
     @EntityGraph(attributePaths = {"candidato", "candidato.formacoes", "candidato.experiencias", "vaga"})
-    List<Candidatura> findByVagaId(Long vagaId);
+    List<Candidaturas> findByVagaId(Long vagaId);
 }
