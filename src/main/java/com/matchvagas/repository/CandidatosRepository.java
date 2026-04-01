49483325ac7f714a948c43aa9d0510ae7bd04fa7
lukejj1 <lucasdiasvieira77@gmail.com>
2026-04-01 10:35:11 -0400

    Create CandidatosRepository.java

---
+++ src/main/java/com/matchvagas/repository/CandidatosRepository.java
@@ -0,0 +1,21 @@
+package com.matchvagas.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matchvagas.backend.entity.CandidatoVaga;
+
+@Repository
+public interface CandidatoVagaRepository extends JpaRepository<CandidatoVaga, Long> {
+
+    Optional<CandidatoVaga> findByEmail(String email);
+
+    List<CandidatoVaga> findByVaga(String vaga);
+
+    List<CandidatoVaga> findByAtivo(Boolean ativo);
+
+    List<CandidatoVaga> findByNomeContainingIgnoreCase(String nome);
+}
