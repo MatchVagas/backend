16b7c93f086d314fff02cd96b186751b088aeb61
lukejj1 <lucasdiasvieira77@gmail.com>
2026-04-08 13:04:25 +0000

    Create ExperienciasResponseDTO.java

---
+++ src/main/java/com/matchvagas/backend/dto/ExperienciasResponseDTO.java
@@ -0,0 +1,23 @@
+package com.matchvagas.backend.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record ExperienciaResponse(
+    Long id,
+    Long candidatoId,
+    String cargo,
+    String empresa,
+    String localTrabalho,
+    LocalDate dataInicio,
+    LocalDate dataFim,
+    Boolean atual,
+    String tipoContrato,
+    String descricao,
+    List<String> responsabilidades,
+    List<String> tecnologias,
+    Double salario,
+    String comprovanteUrl
+) {
+  
+}
