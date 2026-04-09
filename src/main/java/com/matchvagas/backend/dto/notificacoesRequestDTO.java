f9c8451f75a554c919961dc944a00c42f586a157
Romário <romarioroberto@gmail.com>
2026-04-09 19:35:05 -0400

    FIX DTO's

--- src/main/java/com/matchvagas/backend/dto/notificacoesRequestDTO.java
+++ src/main/java/com/matchvagas/backend/dto/notificacoesRequestDTO.java
@@ -1,11 +1,9 @@
 package com.matchvagas.backend.dto;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public record NotificacoesRequestDTO(
