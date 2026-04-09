59f8d542afa4efc086b33d04cd129fa901f3327f
Romário <romarioroberto@gmail.com>
2026-04-09 19:36:45 -0400

    FIX Notificacoes entity

--- src/main/java/com/matchvagas/backend/entity/Notificacao.java
+++ src/main/java/com/matchvagas/backend/entity/Notificacao.java
@@ -1,16 +1,20 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "notificacao");
 public class Notificacao {
 @Id
-@GeneratedValue(stragery = jakarta.persistence.GenerationType.IDENTITY)
+@GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
 private Long  id; 
 
-@Column( titulo = "titulo", nullable = false )
+@Column( name = "titulo", nullable = false )
 private String titulo;
 
-@Column(mensagem = "mensagem" , nullable = false )
-
-
+//@Column(mensagem = "mensagem" , nullable = false )
 
 
 }
